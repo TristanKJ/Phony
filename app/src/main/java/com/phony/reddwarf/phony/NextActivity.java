653b55776c85a18f4fd793b73a3e57e50dd5d0e2
TristanJK <tristanknopejenkins@gmail.com>
2015-11-30 20:21:43 -0800

    Added 2ndary activity

--- app/src/main/java/com/phony/reddwarf/phony/NextActivity.java
+++ app/src/main/java/com/phony/reddwarf/phony/NextActivity.java
@@ -2,6 +2,7 @@ package com.phony.reddwarf.phony;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by RedDwarf on 11/24/2015.
@@ -12,5 +13,19 @@ public class NextActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.next_activity_view);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        updatingUI();
+    }
+
+    private void updatingUI() {
+        int clickCount = Prefs.getClickCounts(this);
+        TextView text = (TextView) findViewById(R.id.click_counter);
+        String textString = String.format(getString(R.string.value_string), clickCount);
+        text.setText(textString);
     }
 }
